3d0695c81c717cb9f710e37e6614c70389427f86
Alexander Black <alexblackjr@gmail.com>
2015-08-11 16:28:15 -0400

    file name changes

---
+++ js/controllers/MainController.js
@@ -0,0 +1,44 @@
+app.controller('MainController',['$scope',
+function($scope){
+    $scope.stores = [
+    {
+     name: '',
+     bigPic: '',
+     subscriptionName: '',
+     price: '',
+     currencyType: '',
+     smallPic1: '',
+     smallPic2: '', 
+     circlePic: '',
+     subscribers: '456',
+     location: '',
+     reviews: ''
+    },
+      {
+     name: '',
+     bigPic: '',
+     subscriptionName: '',
+     price: '',
+     currencyType: '',
+     smallPic1: '',
+     smallPic2: '', 
+     circlePic: '',
+     subscribers: '',
+     location: '',
+     reviews: ''
+    },
+      {
+     name: '',
+     bigPic: '',
+     subscriptionName: '',
+     price: '',
+     currencyType: '',
+     smallPic1: '',
+     smallPic2: '', 
+     circlePic: '',
+     subscribers: '',
+     location: '',
+     reviews: ''
+    }
+    ];
+}]);
